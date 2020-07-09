2c7481c9ba947495a82d3320d4d848142013dcfc
harrry9349 <harrry9349@gmail.com>
2020-07-09 22:28:24 +0900

    1

---
+++ 08_ClassID/js/classid.js
@@ -0,0 +1,6 @@
+// JSを利用してテキストの色を操作する
+document.getElementById("sample").style.color="red";
+document.getElementById("sample").style.backgroundColor="black";
+
+// F12キーを押してMDNを開くとconsoleにid="sample2"を設定した要素のテキストが書かれている
+console.log(document.getElementById("sample2"));
